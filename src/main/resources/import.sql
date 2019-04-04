c5f3f93ef1c2e0e90e63c96bb47a31ac4e034c93
yurtcan <asd@asd.com>
2019-04-04 11:13:52 +0200

    change devadmin password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (id, email, first_name, last_name, password, user_name) VALUES (1, 'devadmin@astronaut.com', 'devadmin', 'devadmin', '$2a$10$.Fo5TYHbd3Hv0gwKyab.rODO1..yGikFDU5JI.aMasy6ETJgSxRly', 'devadmin');
+INSERT INTO users (id, email, first_name, last_name, password, user_name) VALUES (1, 'devadmin@astronaut.com', 'devadmin', 'devadmin', '$2a$10$.9OJva5sUh0f0eSkzakxweYgHZi7slrRrNa2BHD.N8dDNWziBWs2O', 'devadmin');
 INSERT INTO roles (id, name) VALUES (2, 'ROLE_USER');
 INSERT INTO roles (id, name) VALUES (3, 'ROLE_ADMIN');
 INSERT INTO user_roles (user_id, role_id) VALUES (1, 3);
