d3f16d83cfedb5051ee60cccc708e0df744a710a
yurtcan <asd@asd.com>
2019-04-05 13:51:06 +0200

    Introducing logging and more

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-﻿INSERT INTO users (id, email, first_name, last_name, password, user_name, status) VALUES (1, 'devadmin@astronaut.com', 'devadmin', 'devadmin', '$2a$10$.9OJva5sUh0f0eSkzakxweYgHZi7slrRrNa2BHD.N8dDNWziBWs2O', 'devadmin', 'A');
+INSERT INTO users (id, email, first_name, last_name, password, user_name, status) VALUES (1, 'devadmin@astronaut.com', 'devadmin', 'devadmin', '$2a$10$.9OJva5sUh0f0eSkzakxweYgHZi7slrRrNa2BHD.N8dDNWziBWs2O', 'devadmin', 'A');
 INSERT INTO roles (id, name, status) VALUES (2, 'ROLE_USER', 'A');
 INSERT INTO roles (id, name, status) VALUES (3, 'ROLE_ADMIN', 'A');
 INSERT INTO user_roles (user_id, role_id) VALUES (1, 3);
